da3ba7471fc0c65871c71f94aa451f09166b5f97
Maxim Khaylenko <dev.mobile.fl@gmail.com>
2019-09-08 23:11:27 +0300

    parse date on cardview

--- app/src/main/java/com/test/paging/presentation/adapter/RepositoryItemViewHolder.kt
+++ app/src/main/java/com/test/paging/presentation/adapter/RepositoryItemViewHolder.kt
@@ -9,6 +9,10 @@ import androidx.recyclerview.widget.RecyclerView
 import com.test.paging.GlideRequests
 import com.test.paging.R
 import com.test.paging.data.entity.ItemsItem
+import java.text.ParseException
+import java.text.SimpleDateFormat
+import java.util.*
+
 
 class RepositoryItemViewHolder(view: View, private val glide: GlideRequests) : RecyclerView.ViewHolder(view) {
     private val textLogin: TextView = view.findViewById(R.id.text_login)
@@ -22,7 +26,15 @@ class RepositoryItemViewHolder(view: View, private val glide: GlideRequests) : R
 
         textLogin.text = repository?.owner?.login
         textFullname.text = repository?.fullName
-        textDate.text = repository?.owner?.login
+
+        val inputFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.ENGLISH)
+        val outputFormat = SimpleDateFormat("yyyy, MM/dd HH:mm", Locale.ENGLISH)
+        try {
+            val newDate: Date = inputFormat.parse(repository?.createdAt)
+            textDate.text = outputFormat.format(newDate)
+        } catch (e: ParseException) {
+            e.printStackTrace()
+        }
 
         if (repository?.owner?.avatarUrl?.startsWith("http") == true) {
 
