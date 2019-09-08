1c645febdfcf724feb6ace59c86823ea060fe5fd
Maxim Khaylenko <dev.mobile.fl@gmail.com>
2019-09-08 14:52:43 +0300

    implemented ui

--- app/src/main/java/com/test/paging/presentation/view/MainActivity.kt
+++ app/src/main/java/com/test/paging/presentation/view/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.test.paging.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.R.color
+import android.graphics.PorterDuff
 import android.os.Bundle
-import android.util.Log
+import android.widget.EditText
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.RecyclerView
 import com.test.paging.R
 import com.test.paging.domain.usecase.RepositoryGithubUseCase
 import com.test.paging.presentation.base.BaseActivity
 import com.test.paging.presentation.viewmodel.MainViewModel
 import com.test.paging.presentation.viewmodel.MainViewModelFactory
 import javax.inject.Inject
+import androidx.core.graphics.drawable.DrawableCompat
+import android.graphics.drawable.Drawable
 
 class MainActivity : BaseActivity() {
 
@@ -21,14 +26,31 @@ class MainActivity : BaseActivity() {
 
     lateinit var mainViewModel: MainViewModel
 
+    lateinit var editQuery: EditText
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        supportActionBar?.elevation = 0f
+
         mainViewModel = ViewModelProviders.of(this, MainViewModelFactory(repositoryGithubUseCase))[MainViewModel::class.java]
 
-        Log.d("myLogs", "dgr view model: " + repositoryGithubUseCase)
+        editQuery = findViewById(R.id.edit_query)
+        recyclerView = findViewById(R.id.recycler_view)
 
-        mainViewModel.getSomething()
+        setEditQueryTintColor()
+    }
+
+    //for pre-23 API devices
+    private fun setEditQueryTintColor() {
+        val drawableSearch = ContextCompat.getDrawable(this, R.drawable.baseline_search_black_24)
+        if (drawableSearch != null) {
+            val drawable = DrawableCompat.wrap(drawableSearch)
+            DrawableCompat.setTint(drawable, ContextCompat.getColor(this, R.color.colorAccent))
+            DrawableCompat.setTintMode(drawable, PorterDuff.Mode.SRC_IN)
+            editQuery.setCompoundDrawablesWithIntrinsicBounds(drawable, null, null, null)
+        }
     }
 }
